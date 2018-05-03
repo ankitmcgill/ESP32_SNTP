e9dd6c3e980e40c7e0a7d9274ba88b1216c6525f
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2018-05-03 15:50:30 +0530

    added intializing code. tested working

--- ESP32_SNTP.c
+++ ESP32_SNTP.c
@@ -3,6 +3,8 @@
 //
 // USES ESP32 SNTP (THAT USES ESP32 LWIP) TO GET NTP AND MAINTAIN
 // SYSTEM TIME BASED ON THAY
+//  
+// ESP32 SNTP ONLY SUPPORTS 1 NTP SERVER
 //
 // REFERENCE:
 //  (1) https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
@@ -13,12 +15,19 @@
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
 //////////////////////////////////////////////////////////////////
 
-
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "lwip/err.h"
+#include "apps/sntp/sntp.h"
 #include "ESP32_SNTP.h"
 
 
 //INTERNAL VARIABLES
 static bool s_debug;
+
+//NTP  RELATED
+char* s_ntp_server_name;
 
 //INTERNAL FUNCTIONS
 
@@ -29,9 +38,15 @@ void ESP32_WS2812B_SNTP_SetDebug(bool enable)
 	s_debug = enable;
 }
 
-void ESP32_SNTP_Initialize(void)
+void ESP32_SNTP_Initialize(char* ntp_server)
 {
     //INITIALIZE ESP32 SNTP
 
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, ntp_server);
+    s_ntp_server_name = ntp_server;
+    sntp_init();
+
     ets_printf(ESP32_SNTP_TAG" : Initialized\n");
+    ets_printf(ESP32_SNTP_TAG" : NTP Server = %s\n", s_ntp_server_name);
 }
--- include/ESP32_SNTP.h
+++ include/ESP32_SNTP.h
@@ -3,6 +3,8 @@
 //
 // USES ESP32 SNTP (THAT USES ESP32 LWIP) TO GET NTP AND MAINTAIN
 // SYSTEM TIME BASED ON THAY
+//  
+// ESP32 SNTP ONLY SUPPORTS 1 NTP SERVER
 //
 // REFERENCE:
 //  (1) https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
@@ -11,7 +13,7 @@
 //
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 
 #ifndef _ESP32_SNTP_
 #define _ESP32_SNTP_
@@ -23,6 +25,6 @@
 #define ESP32_SNTP_TAG         "ESP32:SNTP"
 
 void ESP32_WS2812B_SNTP_SetDebug(bool enable);
-void ESP32_SNTP_Initialize(void);
+void ESP32_SNTP_Initialize(char* ntp_server);
 
 #endif
