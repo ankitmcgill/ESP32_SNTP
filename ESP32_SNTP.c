fba89cff0daadc0670413a77c3b14aa58ba22d01
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2018-05-05 12:01:30 +0530

    added all the time & timezone functionality. tested working

--- ESP32_SNTP.c
+++ ESP32_SNTP.c
@@ -9,19 +9,20 @@
 // REFERENCE:
 //  (1) https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
 //
+//  (2) TimeZone String List
+//      https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+//
 // MAY 03, 2018
 //
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 
 #include <string.h>
-#include <time.h>
-#include <sys/time.h>
 #include "lwip/err.h"
 #include "apps/sntp/sntp.h"
 #include "ESP32_SNTP.h"
-
+#include "ESP32_UTIL.h"
 
 //INTERNAL VARIABLES
 static bool s_debug;
@@ -49,4 +50,76 @@ void ESP32_SNTP_Initialize(char* ntp_server)
 
     ets_printf(ESP32_SNTP_TAG" : Initialized\n");
     ets_printf(ESP32_SNTP_TAG" : NTP Server = %s\n", s_ntp_server_name);
+}
+
+void ESP32_SNTP_SetTimeZone(char* tz_string)
+{
+    //SET MODULE TIMEZONE USING LINUX STANDARD 
+    //TIMEZONE STRING
+    //http://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+    //https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+
+    setenv("TZ", tz_string, true);
+    tzset();
+
+    if(s_debug)
+    {
+        ets_printf(ESP32_SNTP_TAG" : Timezone set to %s\n", tz_string);
+    }
+}
+
+bool ESP32_SNTP_GetTime(struct tm* retval)
+{
+    //RETURN CURRENT TIME
+
+    time_t now = 0;
+    struct tm timeinfo = {0};
+    uint8_t retry = 0;
+    
+    while(timeinfo.tm_year < (2018 - 1900) &&
+            retry++ < ESP32_SNTP_GET_TIME_RETRY_COUNT)
+    {
+        ESP32_UTIL_DelayBlockingMs(ESP32_SNTP_GET_TIME_RETRY_DELAY_MS);
+        if(s_debug)
+        {
+            ets_printf(ESP32_SNTP_TAG" : getting time attempt %u\n", retry);
+        }
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+
+    if(retry == ESP32_SNTP_GET_TIME_RETRY_COUNT)
+    {
+        //TIME GETTING ERROR
+        if(s_debug)
+        {
+            ets_printf(ESP32_SNTP_TAG" : error ! all attempts finished\n", retry);
+        }
+        return false;
+    }
+
+    //GOT THE TIME
+    //COPY THE VALUES
+    memcpy((void*)retval, (void*)&timeinfo, sizeof(struct tm));
+    return true;
+}
+
+bool ESP32_SNTP_GetTimeString(char* retval, uint8_t retval_size, char* time_fmt)
+{
+    //RETURN CURRENT TIME AS STRING
+
+    struct tm current_time;
+
+    if(!ESP32_SNTP_GetTime(&current_time))
+    {
+        if(s_debug)
+        {
+            ets_printf(ESP32_SNTP_TAG" : error ! could not get current time string\n");
+        }
+        return false;
+    }
+
+    //GOT THE TIME
+    strftime(retval, retval_size, time_fmt, &current_time);
+    return true;
 }
--- include/ESP32_SNTP.h
+++ include/ESP32_SNTP.h
@@ -9,6 +9,9 @@
 // REFERENCE:
 //  (1) https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
 //
+//  (2) TimeZone String List
+//      https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+//
 // MAY 03, 2018
 //
 // ANKIT BHATNAGAR
@@ -18,13 +21,24 @@
 #ifndef _ESP32_SNTP_
 #define _ESP32_SNTP_
 
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 #include "esp_system.h"
 #include "esp_err.h"
 #include "esp_log.h"
 
-#define ESP32_SNTP_TAG         "ESP32:SNTP"
+#define ESP32_SNTP_TAG                      "ESP32:SNTP"
+
+#define ESP32_SNTP_GET_TIME_RETRY_COUNT     (10)
+#define ESP32_SNTP_GET_TIME_RETRY_DELAY_MS  (1500)
 
 void ESP32_WS2812B_SNTP_SetDebug(bool enable);
+
 void ESP32_SNTP_Initialize(char* ntp_server);
+void ESP32_SNTP_SetTimeZone(char* tz_string);
+
+bool ESP32_SNTP_GetTime(struct tm* retval);
+bool ESP32_SNTP_GetTimeString(char* retval, uint8_t retval_size, char* time_fmt);
 
 #endif
